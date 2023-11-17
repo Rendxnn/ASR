9eff6b8ac3c1ffa719acc7b8cd25514e43d3a2c7
JuanJMunozG <jjmunozg@eafit.edu.co>
2023-11-17 13:45:15 -0500

    Ya al agregar al carrito puede mostrar el producto que se añadio

--- ASR_PROJECT/cart/models.py
+++ ASR_PROJECT/cart/models.py
@@ -3,10 +3,12 @@ from django.db import models
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from store.models import Ladrillo
 
 class Cart(models.Model):
+    
     #user = models.ForeignKey(User, on_delete=models.CASCADE)
-    product = models.CharField(max_length=255)
+    product = models.ForeignKey(Ladrillo, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
 
     def __str__(self):
--- ASR_PROJECT/cart/views.py
+++ ASR_PROJECT/cart/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from cart.models import Cart
+from store.models import Ladrillo
 
 def home_cart(request):
     return render(request, 'cart/home_cart.html', {})
@@ -10,13 +11,13 @@ def home_cart(request):
 
 def add_to_cart(request, producto_id):
     cart_item = Cart.objects.filter(pk = producto_id).first()
-
     if cart_item:
         cart_item.quantity += 1
         cart_item.save()
         messages.success(request, "Item added to your cart.")
     else:
-        Cart.objects.create(product=producto_id)
+        instance = Ladrillo.objects.get(pk = producto_id)
+        Cart.objects.create(product=instance)
         messages.success(request, "Item added to your cart.")
 
     return redirect("cart_detail")
@@ -31,7 +32,7 @@ def remove_from_cart(request, cart_item_id):
 
 def cart_detail(request):
     cart_items = Cart.objects.all()
-    total_price = sum(item.quantity * 2 for item in cart_items)
+    total_price = sum(item.quantity * item.product.precio for item in cart_items)
 
     context = {
         "cart_items": cart_items,
